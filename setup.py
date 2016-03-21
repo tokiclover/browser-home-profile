911b1e685c8aaa9fa9fcafe972785f7f9d413f85
tclover <tokiclover@gmail.com>
2016-03-21 15:22:14 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+setup(
+    name = "tmpdir",
+    version = "4.0",
+    packages = ['tmpdir'],
+    scripts = ['bhp.py', 'tmpdirs.py'],
+
+    package_data = {
+        '': ['AUTHORS', 'COPYING', 'README.md', 'ChangeLog'],
+    },
+    exclude_package_data = {
+        'sh': [ '*.sh' ],
+        'pl': [ '*.pl', '*.pm'],
+    },
+
+    author = "tokiclover",
+    author_email = "tokiclover@gmail.com",
+    description = "Temporary Directory Setup with ZRAM support",
+    license = "MIT or 2-clause BSD",
+    keywords = "tmpdir zram tmpfs directory temporary",
+    url = "https://github.com/tokiclover/browser-home-profile",
+    download_url = "https://github.com/tokiclover/browser-home-profile/releases",
+    platform = "Unix (Linux for ZRAM support)"
+)
