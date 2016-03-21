a9f1aecf9bfa6cb246e9228e5512777299eb2cd5
tclover <tokiclover@gmail.com>
2016-03-21 15:17:39 +0100

    bhp.py: 1.4 version bump
    Split out the common helpers to tmpdir/functions.py; and minor
    clean up and documentation update.

--- bhp.py
+++ bhp.py
@@ -3,7 +3,7 @@
 # $Header: bhp.py                                             Exp $
 # $Author: (c) 2016 tokiclover <tokiclover@gmail.com>         Exp $
 # $License: MIT (or 2-clause/new/simplified BSD)              Exp $
-# $Version: 1.2 2016/03/08                                    Exp $
+# $Version: 1.4 2016/03/18                                    Exp $
 #
 
 """This utility manage web-browser home profile directory along with the associated
@@ -26,10 +26,11 @@ be copy/pasted to any project or personal script.
 """
 
 from __future__ import print_function
-import os, os.path, signal, sys, tempfile
+from tmpdir.functions import pr_begin, pr_end, pr_info, pr_warn, pr_error
+from tmpdir.functions import pr_die, eval_colors, mount_info, sigwinch_handler
+import os, os.path, signal, sys, tempfile, tmpdir
 
-bhp_info, color, bg, fg, print_info = dict({}), dict({}), list([]), list([]), dict({})
-print_info['cols'], print_info['len'], print_info['eol'] = 0, 0, ''
+bhp_info = dict({})
 bhp_info['zero'] = os.path.basename(sys.argv[0])
 
 HELP_MESSAGE = 'Usage: %s [OPTIONS] [BROWSER]' % bhp_info['zero']
@@ -39,144 +40,19 @@ HELP_MESSAGE += """
     -t, --tmpdir DIR             Set up a particular TMPDIR
     -p, --profiel PROFILE        Select a particular profile
     -s, --set                    Set up tarball archives
-    -C, --nocolor                Disable colored output
     -h, --help                   Print help message
     -v, --version                Print version message                    
 """
 
-VERSION_STRING = "1.2"
-VERSION_MESSAGE = '%s version %s' % (bhp_info['zero'], VERSION_STRING)
+__author__ = "tokiclover <tokiclover@gmail.com>"
+__date__ = "2016/03/08"
+__version__ = "1.3"
+VERSION_MESSAGE = '%s version %s' % (bhp_info['zero'], __version__)
 
-def tput(cap, conv=0):
-    """Simple helper to querry terminfo(5) capabilities"""
-    TPUT = os.popen('tput %s' % cap)
-    tput = TPUT.read()
-    TPUT.close()
-    if yesno(conv): return int(tput)
-    else: return tput
-
-def sigwinch_handler(sig=signal.SIGWINCH, frame=None):
-    """Handle window resize signal"""
-    global print_info
-    if sys.version[0] == 3:
-        print_info['cols'] = os.get_terminal_size()[0]
-    else:
-        print_info['cols'] = tput('cols', 1)
 signal.signal(signal.SIGWINCH, sigwinch_handler)
 
-def pr_error(msg):
-    """Print error message to stderr"""
-    global print_info
-    print_info['len'] = len(msg)+len(name)+2
-
-    if name:
-        pfx = ' %s%s:%s' % (color['fg-magenta'], name, color['reset'])
-    else:
-        pfx = ''
-    print('%s%s*%s %s %s' % (print_info['eol'], color['fg-red'], color['reset'], pfx, msg),
-                file=sys.stderr)
-
-def pr_die(ret, msg):
-    """Print error message to stderr and exit program"""
-    pr_error(msg)
-    exit(ret)
-
-def pr_info(msg):
-    """Print info message to stdout"""
-    global print_info
-    print_info['len'] = len(msg)+len(name)+2
-
-    if name:
-        pfx = ' %s%s:%s' % (color['fg-yellow'], name, color['reset'])
-    else:
-        pfx = ''
-    print('%s%s*%s %s %s' % (print_info['eol'], color['fg-blue'], color['reset'], pfx, msg),
-                file=sys.stdout)
-
-def pr_warn(msg):
-    """Print warning message to stdout"""
-    global print_info
-    print_info['len'] = len(msg)+len(name)+2
-
-    if name:
-        pfx = ' %s%s:%s' % (color['fg-red'], name, color['reset'])
-    else:
-        pfx = ''
-    print('%s%s*%s %s %s' % (print_info['eol'], color['fg-yellow'], color['reset'], pfx, msg),
-                file=sys.stdout)
-
-def pr_begin(msg):
-    """Print the beginning of a formated message to stdout"""
-    global print_info
-    if print_info['eol'] == '\n': print(print_info['eol'])
-    print_info['eol'] = '\n'
-    print_info['len'] = len(msg)+len(name)+2
-
-    if name:
-        pfx = '%s[%s%s%s]%s' % (color['fg-magenta'], color['fg-blue'], name,
-                color['fg-magenta'], color['reset'])
-    else:
-        pfx = ''
-    print('%s %s' % (pfx, msg), end=' ')
-
-def pr_end(val, msg=''):
-    """Print the end of a formated message to stdout"""
-    global print_info
-    len = print_info['cols'] - print_info['len']
-
-    if val == 0:
-        sfx = '%(fg-blue)s[%(fg-green)sOk%(fg-blue)s]%(reset)s' % color
-    else:
-        sfx = '%(fg-yellow)s[%(fg-red)sNo%(fg-yellow)s]%(reset)s' % color
-    
-    s = '%s %s' % (msg, sfx)
-    print('%*s' % (len, s))
-    print_info['eol'], print_info['len'] = '', 0
-
-def yesno(val=0):
-    """A tiny helper to simplify case incensitive yes/no configuration"""
-    if str(val).lower() in ['0', 'disable', 'off', 'false', 'no']:
-        return 0
-    elif str(val).lower() in ['1', 'enable', 'on', 'true', 'yes']:
-        return 1
-    else:
-        return None
-
-def eval_colors():
-    """Set up colors for output for the print helper family"""
-    global color, bg, fg
-    bc = ['none', 'bold', 'faint', 'italic', 'underline', 'blink',
-        'rapid-blink', 'inverse', 'conceal', 'no-italic', 'no-underline',
-        'no-blink', 'reveal', 'default'
-    ]
-    val = list(range(8))+list(range(23, 25))+[28, '39;49']
-    ESC = '\033['
-    color = { bc[i]: '%s%sm' % (ESC, c) for (i, c) in enumerate(val) }
-    color['reset'] = '%s0m' % ESC
-
-    if tput('colors', 1) >= 256:
-        BG, FG, NUM = '48;5;', '38;5;', 256
-    else:
-        BG, FG, NUM = 4, 3, 8
-
-    bg = [ '%s%s%dm' % (ESC, BG, c) for c in range(NUM) ]
-    fg = [ '%s%s%dm' % (ESC, FG, c) for c in range(NUM) ]
-    bc = ['black', 'red', 'green', 'yellow', 'blue', 'magenta', 'cyan', 'white']
-    for (i, c) in enumerate(bc):
-        color['bg-{0}'.format(c)] = '%s%s%sm' % (ESC, BG, i)
-        color['fg-{0}'.format(c)] = '%s%s%sm' % (ESC, FG, i)
-
-def mount_info(dir):
-    """A tiny helper to simplify probing mounted points"""
-    if not dir: return None
-    MFH = open("/proc/mounts", "r") #pr_die("Failed to open /proc/mounts");
-    for line in MFH:
-        if dir in line: ret = 1; break
-        else: ret = 0
-    MFH.close()
-    return ret
-
 def find_browser(browser=''):
+    """Find a browser to setup"""
     browsers = {
         'mozilla': [ 'aurora', 'firefox', 'icecat', 'seamonkey' ],
         'config':  [ 'conkeror', 'chrome', 'chromium', 'epiphany', 'midory',
@@ -196,6 +72,7 @@ def find_browser(browser=''):
     return 1
 
 def mozilla_profile(browser, profile=''):
+    """Find a Mozilla family browser profile"""
     if profile and os.path.isdir('%s/.mozilla/%s/%s' % (os.environ['HOME'], browser, profile)):
         bhp_info['profile'] = 'mozilla/%s/%s' % (browser, profile)
         return 0
@@ -210,10 +87,8 @@ def mozilla_profile(browser, profile=''):
             break
     PFH.close()
 
-#
-# Use a private initializer function
-#
 def bhp(profile, setup=False):
+    """Profile initializer function and temporary directories setup"""
     global TMPDIR, bhp
     ext = '.tar.%s' % bhp_info['compressor'].split(' ')[0]
 
@@ -267,10 +142,8 @@ def bhp(profile, setup=False):
     
         if setup: bhp_archive(ext, profile)
 
-#
-# Set up or (un)compress archive tarballs accordingly
-#
 def bhp_archive(ext, profile):
+    """Set up or (un)compress archive tarballs accordingly"""
     pr_begin("Setting up tarball... ")
     if os.path.isfile(profile+'/.unpacked'):
         if os.path.isfile(profile+ext):
@@ -311,23 +184,18 @@ def sigalrm_handler(sig=signal.SIGALRM, frame=None):
                 bhp_info['profile'].split('/')[-1])
 signal.signal(signal.SIGALRM, sigalrm_handler)
 
-if sys.version[0] == 3:
-    print_info['cols'] = os.get_terminal_size()[0]
-else:
-    print_info['cols'] = tput('cols', 1)
-
 if __name__ == '__main__':
     bhp_info['browser'], bhp_info['compressor'] = '', 'lz4 -1'
-    profile, setup, name, bhp_info['daemon'] = '', False, bhp_info['zero'], 0
+    profile, setup, bhp_info['daemon'] = '', False, 0
     TMPDIR = os.environ.get('TMPDIR', '/tmp/' + os.environ['USER'])
-    print_info['color'] = os.environ.get('PRINT_COLOR', 1)
+    tmpdir.functions.NAME = bhp_info['zero']
 
     #
     # Set up options according to command line options
     #
     import getopt, re
-    shortopts = 'Cc:d:hp:st:v'
-    longopts  = ['compressor=', 'daemon=', 'noclor', 'help', 'profile=', 'set',
+    shortopts = 'c:d:hp:st:v'
+    longopts  = ['compressor=', 'daemon=', 'help', 'profile=', 'set',
             'tmpdir=', 'version']
     try:
         opts, args = getopt.getopt(sys.argv[1:], shortopts, longopts)
@@ -352,20 +220,8 @@ if __name__ == '__main__':
             TMPDIR = arg
         if opt in ['-d', '--daemon=']:
             bhp_info['daemon'] = arg
-        if opt in ['-C', '--nocolor']:
-            print_info['color'] = 0
-
-    #
-    # Set up colors
-    #
-    if sys.stdout.isatty() and yesno(print_info['color']):
-        eval_colors()
-    else:
-        color = { i: '' for i in ['none', 'bold', 'faint', 'italic', 'underline',
-            'blink', 'rapid-blink', 'inverse', 'conceal', 'no-italic', 'no-underline',
-            'no-blink', 'reveal', 'default',
-            'black', 'red', 'green', 'yellow', 'blue', 'magenta', 'cyan', 'white']
-        }
+        if opt in ['-C', '--noCOLOR']:
+            PRINT_INFO['COLOR'] = 0
 
     #
     # Finally, launch the setup helper
